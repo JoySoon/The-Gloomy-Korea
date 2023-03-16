db8c41ce9f3cdf6ff66d2a5c55c31ea43598e0b0
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-16 10:56:42 +0900

    csv

--- prj/app.py
+++ prj/app.py
@@ -13,54 +13,64 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
+# CSV 파일 업로드
+uploaded_file = st.file_uploader("mastgm0817/The-Gloomy-Korea/prj/출생,결혼.csv", type="csv")
 
-marriage_df = pd.read_csv("/content/출생,결혼.csv", index_col=0)
-students_df = pd.read_csv("/content/학생수.csv", index_col=0)
-close_school_df = pd.read_csv("/content/학교.csv", index_col=0)
-# close_school_df
-# students_df
-# marriage_df
+# 업로드된 파일이 있다면
+if uploaded_file is not None:
+    # pandas를 사용하여 데이터프레임으로 변환
+    df = pd.read_csv(uploaded_file)
+    # 데이터프레임 출력
+    st.write(df)
 
-marriage_df.rename_axis("연도", axis='index', inplace=True) # 인덱스의 이름을 바꾸는것
-m_p = marriage_df['항목'] == '결혼(건)'
-df = marriage_df[m_p][['월','값']]
-df_month = df.copy()
-df_month['월'] = df_month['월'].str.replace('월', '').astype(int)
-df_year = df_month.sort_values(['연도','월'], ascending=True).copy()
-reset_df = df_year.reset_index()
-reset_df = reset_df[['연도','값']]
-m_sum_df = reset_df.groupby(by=['연도']).sum()
-# m_sum_df
 
-df_by_year = lambda year: reset_df.loc[reset_df['연도'] == year].copy()
-month_and_year = lambda year: (df_by_year(year)['월'], df_by_year(year)['값'])
+# marriage_df = pd.read_csv("/content/출생,결혼.csv", index_col=0)
+# students_df = pd.read_csv("/content/학생수.csv", index_col=0)
+# close_school_df = pd.read_csv("/content/학교.csv", index_col=0)
+# # close_school_df
+# # students_df
+# # marriage_df
 
-plt.figure(figsize=(10, 12))
-draw_plot = lambda year: plt.plot(*month_and_year(year), '*:', label=f'{year}년')
+# marriage_df.rename_axis("연도", axis='index', inplace=True) # 인덱스의 이름을 바꾸는것
+# m_p = marriage_df['항목'] == '결혼(건)'
+# df = marriage_df[m_p][['월','값']]
+# df_month = df.copy()
+# df_month['월'] = df_month['월'].str.replace('월', '').astype(int)
+# df_year = df_month.sort_values(['연도','월'], ascending=True).copy()
+# reset_df = df_year.reset_index()
+# reset_df = reset_df[['연도','값']]
+# m_sum_df = reset_df.groupby(by=['연도']).sum()
+# # m_sum_df
 
-n_sub_plot = [221,222,223,224]
+# df_by_year = lambda year: reset_df.loc[reset_df['연도'] == year].copy()
+# month_and_year = lambda year: (df_by_year(year)['월'], df_by_year(year)['값'])
 
-def make_subplot(i,n):
-  c_year = 2010+n
-  plt.subplot(n_sub_plot[i//3])
-  a_year = [v for v in reset_df['연도'].unique()]
-  a_year.sort()
-  if n == 9:
-    y_data = [v for v in a_year[n:n+4]]
-    plt.title(f'{c_year}년 부터 {c_year+3}년 까지 ')
-  else: 
-    y_data = [v for v in a_year[n:n+3]]
-    plt.title(f'{c_year}년 부터 {c_year+2}년 까지')
-  y_data.sort()
-  for index in y_data:
-    draw_plot(index)
-  plt.xlabel('월')
-  plt.ylabel('결혼 건수')
-  plt.grid(True)
-  plt.legend(loc=0)
+# plt.figure(figsize=(10, 12))
+# draw_plot = lambda year: plt.plot(*month_and_year(year), '*:', label=f'{year}년')
 
-for i in range(0,10,3):
-  make_subplot(i,i)
+# n_sub_plot = [221,222,223,224]
 
-plt.tight_layout() # 안 겹치게 레이아웃 조정
-plt.show()
+# def make_subplot(i,n):
+#   c_year = 2010+n
+#   plt.subplot(n_sub_plot[i//3])
+#   a_year = [v for v in reset_df['연도'].unique()]
+#   a_year.sort()
+#   if n == 9:
+#     y_data = [v for v in a_year[n:n+4]]
+#     plt.title(f'{c_year}년 부터 {c_year+3}년 까지 ')
+#   else: 
+#     y_data = [v for v in a_year[n:n+3]]
+#     plt.title(f'{c_year}년 부터 {c_year+2}년 까지')
+#   y_data.sort()
+#   for index in y_data:
+#     draw_plot(index)
+#   plt.xlabel('월')
+#   plt.ylabel('결혼 건수')
+#   plt.grid(True)
+#   plt.legend(loc=0)
+
+# for i in range(0,10,3):
+#   make_subplot(i,i)
+
+# plt.tight_layout() # 안 겹치게 레이아웃 조정
+# plt.show()
