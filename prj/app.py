bec3878a141d36d405dfcd1e51cc3da118f69d35
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-17 01:22:26 +0900

    boom

--- prj/app.py
+++ prj/app.py
@@ -5,24 +5,25 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 
 # 텍스트 모음
-title = "한국의 미래 :boom:"
+title = "한국의 미래"
 text = "(2011년 - 2022년)"
 year_text = "연도 선택"
 chart_text = "차트 선택"
 
 st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>{title}</div>", unsafe_allow_html=True)
+st.markdown(" :boom: ")
 st.markdown(f"<div style='text-align:center; font-size:24px'>{text}</div>", unsafe_allow_html=True)
 st.markdown("---")
 years = np.arange(2011,2023)
 chart = ['학생','폐교','출생 및 결혼','폐교(파이)']
 
-st.sidebar.markdown("---")
 st.sidebar.markdown(f"<div style='text-align:center; font-weight:bold; font-size:18px'>{year_text}</div>", unsafe_allow_html=True)
+st.sidebar.markdown("---")
 year = st.sidebar.selectbox(
     "",
     years)
-st.sidebar.markdown("---")
 st.sidebar.markdown(f"<div style='text-align:center; font-weight:bold; font-size:18px;'>{chart_text}</div>", unsafe_allow_html=True)
+st.sidebar.markdown("---")
 with st.sidebar:
     option = st.radio(
         "",
