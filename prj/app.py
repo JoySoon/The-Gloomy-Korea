e807d3ad530b800d515aa921965e4c2c457259da
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-16 13:32:43 +0900

    test

--- prj/app.py
+++ prj/app.py
@@ -2,98 +2,67 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
 
 st.title("통계자료로 보는 한국의 어두운 전망")
 st.title("(2010년 - 2022년)")
 
 
-# chart_data = pd.DataFrame(
-#     np.random.randn(20, 3),
-#     columns=['a', 'b', 'c'])
 
-# st.line_chart(chart_data)
+# def view_year(i):
+  
 
+# 데이터 커스터마이징징
+close_school_df = pd.read_csv("prj/app.py", index_col=0)
+area_number = len(close_school_df['지역'].unique()[1:])
+not_all_area = close_school_df[close_school_df['지역'] != '전국']
+sorted_area = not_all_area[['지역','당년(개)']]
+all_area = close_school_df[close_school_df['지역'] == '전국']
+sorted_area['전체평균'] = round(all_area['당년(개)'] / area_number).astype(int)
+set_index_area = sorted_area.reset_index()
+set_index_area.set_index('지역')
 
-
-marriage_df = pd.read_csv("prj/출생,결혼.csv", index_col=0)
-students_df = pd.read_csv("prj/학생수.csv", index_col=0)
-close_school_df = pd.read_csv("prj/학교.csv", index_col=0)
-# # close_school_df
-# # students_df
-# # marriage_df
-
-marriage_df.rename_axis("연도", axis='index', inplace=True) # 인덱스의 이름을 바꾸는것
-m_p = marriage_df['항목'] == '결혼(건)'
-df = marriage_df[m_p][['월','값']]
-df_month = df.copy()
-df_month['월'] = df_month['월'].str.replace('월', '').astype(int)
-df_year = df_month.sort_values(['연도','월'], ascending=True).copy()
-reset_df = df_year.reset_index()
-reset_df = reset_df[['연도','값']]
-m_sum_df = reset_df.groupby(by=['연도']).sum()
-
-b_p = marriage_df['항목'] == '출생(명)'
-df = marriage_df[b_p][['월','값']]
-df_month = df.copy()
-df_month['월'] = df_month['월'].str.replace('월', '').astype(int)
-df_year = df_month.sort_values(['연도','월'], ascending=True).copy()
-reset_df = df_year.reset_index()
-b_sum_df = reset_df[['연도','값']]
-b_sum_df = b_sum_df.groupby(by=['연도']).sum()
+data_2022 = set_index_area[set_index_area['날짜'] == 2022]
 
 
-merged_df = pd.merge(m_sum_df, b_sum_df, on='연도', how='outer')
-merged_df = merged_df.rename(columns={'값_x': '결혼(수)', '값_y': '출생(수)'})
+# 차트 만들기기
+fig = ff.create_table(data_2022, height_constant=60)
+# Add graph data
+team = data_2022['지역']
+each_area_count = data_2022['당년(개)']
+average_count = data_2022['전체평균']
 
+trace1 = go.Bar(x=team, y=each_area_count, xaxis='x2', yaxis='y2',
+                marker=dict(color='#0099ff'),
+                name='Each close')
+trace2 = go.Bar(x=team, y=average_count, xaxis='x2', yaxis='y2',
+                marker=dict(color='#404040'),
+                name='Total Average')
 
-st.line_chart(merged_df)
+# Add trace data to figure
+fig.add_traces([trace1, trace2])
 
+# initialize xaxis2 and yaxis2
+fig['layout']['xaxis2'] = {}
+fig['layout']['yaxis2'] = {}
 
+# Edit layout for subplots
+fig.layout.yaxis.update({'domain': [0, .45]})
+fig.layout.yaxis2.update({'domain': [.6, 1]})
 
-# 데이터 생성
-x = [1, 2, 3, 4, 5]
-y = [1, 4, 9, 16, 25]
+# The graph's yaxis2 MUST BE anchored to the graph's xaxis2 and vice versa
+fig.layout.yaxis2.update({'anchor': 'x2'})
+fig.layout.xaxis2.update({'anchor': 'y2'})
+fig.layout.yaxis2.update({'title': 'Goals'})
 
-# 그래프 생성
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers'))
+# Update the margins to add a title and see graph x-labels.
+fig.layout.margin.update({'t':75, 'l':50})
+fig.layout.update({'title': '2022 학교 폐교율'})
 
-# 레이아웃 설정
-fig.update_layout(title='My Line Chart', xaxis_title='X-axis', yaxis_title='Y-axis')
+# Update the height because adding a graph vertically will interact with
+# the plot height calculated for the table
+fig.layout.update({'height':800})
 
-# 그래프 출력
-st.plotly_chart(fig)
-
-# df_by_year = lambda year: reset_df.loc[reset_df['연도'] == year].copy()
-# month_and_year = lambda year: (df_by_year(year)['월'], df_by_year(year)['값'])
-
-# plt.figure(figsize=(10, 12))
-# draw_plot = lambda year: plt.plot(*month_and_year(year), '*:', label=f'{year}년')
-
-# n_sub_plot = [221,222,223,224]
-
-# def make_subplot(i,n):
-#   c_year = 2010+n
-#   plt.subplot(n_sub_plot[i//3])
-#   a_year = [v for v in reset_df['연도'].unique()]
-#   a_year.sort()
-#   if n == 9:
-#     y_data = [v for v in a_year[n:n+4]]
-#     plt.title(f'{c_year}년 부터 {c_year+3}년 까지 ')
-#   else: 
-#     y_data = [v for v in a_year[n:n+3]]
-#     plt.title(f'{c_year}년 부터 {c_year+2}년 까지')
-#   y_data.sort()
-#   for index in y_data:
-#     draw_plot(index)
-#   plt.xlabel('월')
-#   plt.ylabel('결혼 건수')
-#   plt.grid(True)
-#   plt.legend(loc=0)
-
-# for i in range(0,10,3):
-#   make_subplot(i,i)
-
-# plt.tight_layout() # 안 겹치게 레이아웃 조정
-# plt.show()
+# Plot!
+fig.show()
